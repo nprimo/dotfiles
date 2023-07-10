6b98bf72ec783e4b1cc6449ad82d6906df8f75c3
nprimo <primo.niccolo@gmail.com>
2023-07-10 18:40:40 +0100

    feat(nvim_nprimo): add AI autocomplete plugin

--- nvim_nprimo/lua/nprimo/packer.lua
+++ nvim_nprimo/lua/nprimo/packer.lua
@@ -81,6 +81,18 @@ return require('packer').startup(function()
         }
     }
 
+    -- codeium - AI autocomplete
+    use {
+        'Exafunction/codeium.vim',
+        config = function()
+            -- Change '<C-g>' here to any keycode you like.
+            vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+            vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+            vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+            vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+        end
+    }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
