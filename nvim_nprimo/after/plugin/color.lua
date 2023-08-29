30068932f980916fa9265a2076f19b3b4e2654de
nprimo <primo.niccolo@gmail.com>
2023-08-29 21:19:44 +0100

    feat(nvim): update color theme

--- nvim_nprimo/after/plugin/color.lua
+++ nvim_nprimo/after/plugin/color.lua
@@ -3,6 +3,9 @@ vim.g.disable_background = true
 function ColorMyPencils(color)
     color = color or "base16-tomorrow-night-eighties"
     vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
