7b3ce1e413eee8befdf269f6f8779921c814d715
nprimo <primo.niccolo@gmail.com>
2023-12-22 11:58:33 +0000

    feat(nvim): remove nvim-web-devicons

--- nvim_nprimo/lua/nprimo/packer.lua
+++ nvim_nprimo/lua/nprimo/packer.lua
@@ -93,7 +93,6 @@ return require('packer').startup(function()
         branch = "v3.x",
         requires = {
             "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
             -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         }
