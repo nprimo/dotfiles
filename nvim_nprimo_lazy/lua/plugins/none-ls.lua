d6aece8e647e5d9286c2f576f4167333ef546989
nprimo <primo.niccolo@gmail.com>
2023-12-28 17:44:59 +0000

    feat(nvim): update null_ls config

--- nvim_nprimo_lazy/lua/plugins/none-ls.lua
+++ nvim_nprimo_lazy/lua/plugins/none-ls.lua
@@ -7,6 +7,7 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.gofumpt,
+                null_ls.builtins.diagnostics.actionlint,
             },
         })
     end,
