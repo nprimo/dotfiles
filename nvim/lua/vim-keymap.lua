2e81d5516983cc0f05cd4ed765280f4496edb7e4
nprimo <primo.niccolo@gmail.com>
2024-01-11 09:25:46 +0100

    feat(nvim): remove noetree and add keymap for netrw

--- nvim/lua/vim-keymap.lua
+++ nvim/lua/vim-keymap.lua
@@ -28,3 +28,5 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- Open netwr
+vim.keymap.set('n', '<C-n>', ':Lexplore<CR>')
