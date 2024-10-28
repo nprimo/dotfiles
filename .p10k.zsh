8b06ea9259a10df2852fa643b56c02f6d1723be4
niccolo <niccolo@cloudflare.com>
2024-10-28 09:39:27 +0000

    feat(p10k): add information about kubecontext in prompt

--- .p10k.zsh
+++ .p10k.zsh
@@ -52,6 +52,7 @@
     context                   # user@host
     dir                       # current directory
     vcs                       # git status
+    kubecontext               # context for kubectl commands
     command_execution_time    # previous command duration
     # =========================[ Line #2 ]=========================
     newline                   # \n
