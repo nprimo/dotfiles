de3230eca78b7e7054d4db05b206d7a645ca69fe
nprimo <primo.niccolo@gmail.com>
2025-02-22 20:00:21 +0000

    feat[nvim]: add go snippet

--- .config/nvim/lua/custom/completion.lua
+++ .config/nvim/lua/custom/completion.lua
@@ -19,18 +19,17 @@ cmp.setup({
 				luasnip.expand_or_jump()
 			end
 		end, { "i", "s" }),
-
-		-- ["<C-j>"] = cmp.mapping(function()
-		-- 	if luasnip.locally_jumpable(-1) then
-		-- 		luasnip.jump(-1)
-		-- 	end
-		-- end, { "i", "s" }),
+		["<C-j>"] = cmp.mapping(function()
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { "i", "s" }),
 	}),
+
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "path" },
 		{ name = "source" },
-		{ name = "supermaven" },
 	},
 })
