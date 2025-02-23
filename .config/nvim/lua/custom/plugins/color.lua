83c122b5271012547ed027ade95e353b74147a2a
nprimo <primo.niccolo@gmail.com>
2025-02-23 13:19:24 +0000

    feat[nvim]: remove lazygit + update colorscheme

--- .config/nvim/lua/custom/plugins/color.lua
+++ .config/nvim/lua/custom/plugins/color.lua
@@ -5,7 +5,7 @@ return {
 		priority = 1000,
 		opts = {},
 		init = function()
-			vim.cmd.colorscheme("tokyonight-moon")
+			vim.cmd.colorscheme("tokyonight-night")
 			vim.cmd.hi("Comment gui=none")
 		end,
 	},
