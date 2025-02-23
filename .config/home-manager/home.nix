40f3d9fc71f881ebc7d4c4c7dd0d1c2025b6a628
nprimo <primo.niccolo@gmail.com>
2025-02-23 22:23:34 +0000

    feat[home-manager]: add simple home-manager

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "nprimo";
+  home.homeDirectory = "/home/nprimo";
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    beamMinimal27Packages.elixir_1_17
+    nodejs_23
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  programs.home-manager.enable = true;
+}
