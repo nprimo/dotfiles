6a22233b16550f9928101b85ceac264a4ed0b1b8
nprimo <primo.niccolo@gmail.com>
2025-03-17 21:10:25 +0000

    feat[nvim]: update dump

--- .config/nvim/lua/custom/plugins/dump.lua
+++ .config/nvim/lua/custom/plugins/dump.lua
@@ -1,8 +1,18 @@
 return {
+	-- "nprimo/dump.nvim",
 	dir = "~/nprimo/dump.nvim",
 	config = function()
-		require("dump").setup({
-			name = "nico",
-		})
+		local dump = require("dump")
+		dump.setup()
+
+		vim.keymap.set("n", "<leader>dn", function()
+			dump.new()
+		end, { desc = "Open new dump file" })
+		vim.keymap.set("n", "<leader>dl", function()
+			dump.list()
+		end, { desc = "Open dump files list" })
+		vim.keymap.set("n", "<leader>da", function()
+			dump.archive()
+		end, { desc = "Open dump files list to archive" })
 	end,
 }
