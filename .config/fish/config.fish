8d5738f5d42587a64038a17243bcd4e6e6e7fc7d
nprimo <primo.niccolo@gmail.com>
2025-12-07 14:25:22 +0000

    feat[fish]: add bun autocomplete

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,7 @@ set ENCORE_INSTALL "$HOME/.encore"
 set PATH $PATH "$ENCORE_INSTALL/bin"
 
 source $HOME/.config/fish/alias.fish
+source $HOME/.config/fish/bun.fish
 set -U fish_greeting
 
 ##################
