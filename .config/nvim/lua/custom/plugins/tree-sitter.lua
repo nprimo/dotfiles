95a3b689a913ec792c05dd8893f088c1ccbd5579
nprimo <primo.niccolo@gmail.com>
2026-06-18 12:20:18 +0100

    feat[nvim]: update treesitter to point to latest

--- .config/nvim/lua/custom/plugins/tree-sitter.lua
+++ .config/nvim/lua/custom/plugins/tree-sitter.lua
@@ -1,6 +1,6 @@
 return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
+	branch = "main",
 	build = ":TSUpdate",
 	-- main = "nvim-treesitter.configs", -- Sets main module to use for opts
 	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -20,14 +20,7 @@ return { -- Highlight, edit, and navigate code
 		},
 		-- Autoinstall languages that are not installed
 		auto_install = true,
-		highlight = {
-			enable = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
-		},
-		indent = { enable = true, disable = { "ruby" } },
+		indent = { enable = true },
 	},
 	-- There are additional nvim-treesitter modules that you can use to interact
 	-- with nvim-treesitter. You should go explore a few and see what interests you:
